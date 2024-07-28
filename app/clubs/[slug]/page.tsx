d3d343a5a120b124823252d7dd2ce1ab5f4a7028
Isaac Wong <47isaacw@gmail.com>
2024-07-28 11:35:17 -0400

    ability to add tags from admin

--- app/clubs/[slug]/page.tsx
+++ app/clubs/[slug]/page.tsx
@@ -1,118 +1,138 @@
-'use client'
+"use client";
 import { collection, query, where } from "firebase/firestore";
-import db from '@/firebase/firestore/firestore';
+import db from "@/firebase/firestore/firestore";
 import { useParams } from "next/navigation";
-import {useEffect, useState} from 'react';
-import {doc, getDoc, getDocs} from '@firebase/firestore';
-import {setLazyProp} from 'next/dist/server/api-utils';
-import Register from '@/components/clubs/Register';
-import {getAuth, onAuthStateChanged} from 'firebase/auth';
-import AdminMenu from '@/app/clubs/[slug]/AdminMenu';
-import Loading from '@/components/Loading';
-import MemberName from '@/components/MemberName';
-import Description from '@/app/clubs/[slug]/Description';
-import Tags from '@/components/clubs/Tags';
-import Admins from '@/app/clubs/[slug]/Admins';
+import { useEffect, useState } from "react";
+import { doc, getDoc, getDocs } from "@firebase/firestore";
+import { setLazyProp } from "next/dist/server/api-utils";
+import Register from "@/components/clubs/Register";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import AdminMenu from "@/app/clubs/[slug]/AdminMenu";
+import Loading from "@/components/Loading";
+import MemberName from "@/components/MemberName";
+import Description from "@/app/clubs/[slug]/Description";
+import Tags from "@/components/clubs/Tags";
+import Admins from "@/app/clubs/[slug]/Admins";
 
 export default function Page({ params }: { params: { slug: string } }) {
+  const [club, setClub] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [clubNotFound, setClubNotFound] = useState(false);
+  const [clubId, setClubId] = useState();
+  const [admins, setAdmins] = useState([]);
+  const [adminIds, setAdminIds] = useState([]);
 
-	const [club, setClub] = useState({});
-	const [loading, setLoading] = useState(true);
-	const [clubNotFound, setClubNotFound] = useState(false);
-	const [clubId, setClubId] = useState();
-	const [admins, setAdmins] = useState([]);
-	const [adminIds, setAdminIds] = useState([]);
+  const [uid, setUid] = useState();
+  const [adminMenuOpened, setAdminMenuOpened] = useState(false);
 
-	const [uid, setUid] = useState()
-	const [adminMenuOpened, setAdminMenuOpened] = useState(false)
+  const [editedDescription, setEditedDescription] = useState();
 
-	const [editedDescription, setEditedDescription] = useState()
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // @ts-ignore
+      setUid(user.uid);
+    }
+  });
 
-	const auth = getAuth();
-	onAuthStateChanged(auth, (user) => {
-		if (user) {
+  async function searchClubInfo() {
+    const docSnap = await getDoc(doc(db, "clubs", params.slug));
 
-			// @ts-ignore
-			setUid(user.uid);
+    let hold: any = [];
+    docSnap.data();
+    setClubId(docSnap.id);
+    setClub(docSnap.data());
+    setEditedDescription(club["description"]);
+    setAdminIds(docSnap.data()["admins"]);
+  }
 
-		}
-	});
+  async function searchAdmins() {
+    const docSnap = await getDocs(
+      collection(db, `clubs/${params.slug}/members`)
+    );
 
-	async function searchClubInfo() {
-		const docSnap = await getDoc(doc(db, 'clubs', params.slug));
+    let hold: any = [];
+    let adminIdHold: string[] = [];
+    docSnap.forEach((doc) => {
+      if (doc.data().role === "admin") {
+        hold.push({ uid: doc.data()["uid"], title: doc.data()["title"] });
+        adminIdHold.push(doc.data()["uid"]);
+      }
+    });
 
-		let hold:any = []
-		docSnap.data()
-		setClubId(docSnap.id)
-		setClub(docSnap.data())
-		setEditedDescription(club['description'])
-		setAdminIds(docSnap.data()['admins'])
-	}
+    setAdmins(hold);
+    setAdminIds(adminIdHold);
+    setLoading(false);
+  }
 
-	async function searchAdmins() {
-		const docSnap = await getDocs(collection(db, `clubs/${params.slug}/members`));
+  useEffect(() => {
+    searchAdmins();
+  }, []);
 
-		let hold:any = []
-		let adminIdHold:string[] = []
-		docSnap.forEach((doc) => {
-			if (doc.data().role === 'admin') {
-				hold.push({uid: doc.data()['uid'], title: doc.data()['title']})
-				adminIdHold.push(doc.data()['uid'])
-			}
-		})
+  useEffect(() => {
+    searchClubInfo();
+  }, [adminMenuOpened]);
 
-			setAdmins(hold)
-			setAdminIds(adminIdHold)
-			setLoading(false)
+  return (
+    <section>
+      {clubNotFound ? (
+        <h1>Club Not Found</h1>
+      ) : (
+        <div>
+          {loading ? (
+            <Loading />
+          ) : (
+            <div className={"grid grid-cols-2 mx-24 xl:mx-48 gap-4 lg:gap-0"}>
+              <div>
+                <h1 className={"text-4xl"}>{club["name"]}</h1>
+                <img
+                  className={"rounded-xl w-96"}
+                  src={club["logo"]}
+                  alt={`${club["name"]} logo`}
+                />
+              </div>
 
-		}
+              {/*info*/}
+              <div className={"mt-[2.5rem]"}>
+                <Description
+                  adminMenuOpened={adminMenuOpened}
+                  desc={club["description"]}
+                  clubId={club["url"]}
+                />
 
-	useEffect(() => {
-		searchAdmins()
-	}, []);
+                <Admins
+                  admins={admins}
+                  adminMenuOpened={adminMenuOpened}
+                  clubId={club["url"]}
+                />
 
-	useEffect(() => {
-		searchClubInfo()
-	}, [adminMenuOpened]);
+                <Tags
+                  tagList={club["tags"]}
+                  clubId={club["url"]}
+                  adminMenuOpened={adminMenuOpened}
+                />
+              </div>
 
-	return (
-
-		<section>
-			{clubNotFound ? <h1>Club Not Found</h1> :
-				<div>
-					{loading ? <Loading/>:
-						<div className={'grid grid-cols-2 mx-24 xl:mx-48 gap-4 lg:gap-0'}>
-							<div>
-								<h1 className={'text-4xl'}>{club['name']}</h1>
-								<img className={'rounded-xl w-96'} src={club['logo']} alt={`${club['name']} logo`}/>
-							</div>
-
-							{/*info*/}
-							<div className={'mt-[2.5rem]'}>
-								<Description adminMenuOpened={adminMenuOpened} desc={club['description']} clubId={club['url']}/>
-
-								<Admins admins={admins} adminMenuOpened={adminMenuOpened} clubId={club['url']}/>
-
-								<Tags tagList={club['tags']}/>
-							</div>
-
-							<div>
-							{adminIds.indexOf(uid) !== -1 ? <button onClick={() => {setAdminMenuOpened(!adminMenuOpened)}} className={'border-2 p-2 bg-amber-300'}>Edit Club</button> : <></>}
-							{adminMenuOpened ? <AdminMenu clubId={clubId}/> : ''}
-							<Register clubId={clubId}/>
-							</div>
-
-						</div>
-
-
-					}
-
-				</div>
-			}
-
-
-
-
-		</section>
-	)
+              <div>
+                {adminIds.indexOf(uid) !== -1 ? (
+                  <button
+                    onClick={() => {
+                      setAdminMenuOpened(!adminMenuOpened);
+                    }}
+                    className={"border-2 p-2 bg-amber-300"}
+                  >
+                    Edit Club
+                  </button>
+                ) : (
+                  <></>
+                )}
+                {adminMenuOpened ? <AdminMenu clubId={clubId} /> : ""}
+                <Register clubId={clubId} />
+              </div>
+            </div>
+          )}
+        </div>
+      )}
+    </section>
+  );
 }
--- components/clubs/Tags.tsx
+++ components/clubs/Tags.tsx
@@ -1,7 +1,59 @@
-export default function Tags(props: { tagList: any[] }) {
-	return <>
-		{props.tagList.map((tag, i:number) => {return <p key={i}
-														className={'rounded-3xl border-2 bg-yellow-200 inline-block w-fit px-4 py-1'}>{tag['label']}</p>
-		})}
-	</>
+import { PlusSquare } from "react-feather";
+import Select from "react-select";
+import tagList from "@/components/tags";
+import React, { useState } from "react";
+import { doc, updateDoc } from "@firebase/firestore";
+import db from "@/firebase/firestore/firestore";
+export default function Tags(props: {
+  tagList: any[];
+  adminMenuOpened: boolean;
+}) {
+  const [tags, setTags] = useState([]);
+
+  async function handleSubmit(e: { preventDefault: () => void }) {
+    e.preventDefault();
+    await updateDoc(doc(db, "clubs", props.clubId), {
+      tags: tags,
+    })
+      .then(() => alert("Tags updated!"))
+      .catch((error) => alert("Error updating tag: " + error));
+  }
+
+  if (!props.adminMenuOpened) {
+    return (
+      <div>
+        {props.tagList.map((tag, i: number) => {
+          return (
+            <p
+              key={i}
+              className={
+                "rounded-3xl border-2 bg-yellow-200 inline-block w-fit px-4 py-1"
+              }
+            >
+              {tag["label"]}
+            </p>
+          );
+        })}
+      </div>
+    );
+  } else
+    return (
+      <div>
+        <div className={"inline"}>
+          <Select
+            isMulti
+            isSearchable
+            isClearable
+            isOptionDisabled={() => tags.length >= 4}
+            options={tagList}
+            defaultValue={props.tagList}
+            onChange={(e) => setTags(e)}
+          />
+          <p>Select up to 4.</p>
+          <button className={"border-2 p-2 w-fit"} onClick={handleSubmit}>
+            Save
+          </button>
+        </div>
+      </div>
+    );
 }
