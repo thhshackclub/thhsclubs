a9dfd32ba4878f3e9bbf02fdf6dfdba8875c8fd5
Isaac Wong <47isaacw@gmail.com>
2024-08-08 19:29:03 -0400

    Fixed names not showing up

--- components/MemberName.tsx
+++ components/MemberName.tsx
@@ -20,10 +20,11 @@ export default function MemberName(props: {
       const querySnapshot = await getDocs(q);
       querySnapshot.forEach((doc) => {
         // doc.data() is never undefined for query doc snapshots
-        // console.log(doc.id, " => ", doc.data());
+        // console.log(doc.data().name["fName"] + " " + doc.data().name["lName"]);
         setName(doc.data().name["fName"] + " " + doc.data().name["lName"]);
       });
     }
+    read();
   });
   return <>{name}</>;
 }
